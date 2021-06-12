cb8bb5eeb3bb21dc8c4aa5a8dc8a7a3510b388b4
Konstantin Voykov <koykov0@gmail.com>
2021-06-12 23:54:42 +0300

    Improve urlvector tests.

--- urlvector/urlvector_test.go
+++ urlvector/urlvector_test.go
@@ -19,22 +19,25 @@ func BenchmarkParseUrlvector(b *testing.B) {
 			reportErr(b, "scheme", "http", vec.SchemeString())
 		}
 		if vec.UsernameString() != "user" {
-			reportErr(b, "scheme", "user", vec.UsernameString())
+			reportErr(b, "username", "user", vec.UsernameString())
 		}
-		if vec.PasswordString() != "password" {
-			reportErr(b, "scheme", "password", vec.PasswordString())
+		if vec.PasswordString() != "pass" {
+			reportErr(b, "password", "pass", vec.PasswordString())
 		}
 		if vec.HostString() != "[1080:0:0:0:8:800:200C:417A]:61616" {
-			reportErr(b, "scheme", "[1080:0:0:0:8:800:200C:417A]:61616", vec.HostString())
+			reportErr(b, "host", "[1080:0:0:0:8:800:200C:417A]:61616", vec.HostString())
 		}
 		if vec.PathString() != "/foo" {
-			reportErr(b, "scheme", "/foo", vec.PathString())
+			reportErr(b, "path", "/foo", vec.PathString())
 		}
 		if vec.HashString() != "#foobar" {
-			reportErr(b, "scheme", "#foobar", vec.HashString())
+			reportErr(b, "hash", "#foobar", vec.HashString())
 		}
 		if vec.Query().Get("page").String() != "https://ultra-software-base.ru/system/google-chrome.html?yclid=212247430717539672" {
 			b.Error("query[page] mismatch")
+		}
+		if vec.Query().Get("ua").String() != "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.105 YaBrowser/21.3.3.230 Yowser/2.5 Safari/537.36" {
+			b.Error("query[ua] mismatch")
 		}
 
 		urlvector.Release(vec)
