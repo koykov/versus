aa7bc8d2197eecffb591d153d9bd5bdaf1a61b07
Konstantin Voykov <koykov0@gmail.com>
2021-11-03 17:22:49 +0200

    bytealg versus project: use subtests.

--- bytealg/bytealg_test.go
+++ bytealg/bytealg_test.go
@@ -2,6 +2,7 @@ package bytealg
 
 import (
 	"bytes"
+	"strings"
 	"testing"
 
 	"github.com/koykov/bytealg"
@@ -13,24 +14,50 @@ var (
 	trimExpect = []byte("foo bar")
 	trimCutStr = "?!."
 	trimCut    = []byte(trimCutStr)
+
+	trimOriginS = "..foo bar!!???"
+	trimExpectS = "foo bar"
+	trimCutS    = "?!."
 )
 
-func BenchmarkTrim_Bytealg(b *testing.B) {
-	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
-		r := bytealg.Trim(trimOrigin, trimCut)
-		if !bytes.Equal(r, trimExpect) {
-			b.Errorf(`Trim: mismatch result %s and expectation %s`, fastconv.B2S(r), fastconv.B2S(trimExpect))
+func BenchmarkBytealg(b *testing.B) {
+	b.Run("trim", func(b *testing.B) {
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			r := bytealg.Trim(trimOrigin, trimCut)
+			if !bytes.Equal(r, trimExpect) {
+				b.Errorf(`Trim: mismatch result %s and expectation %s`, fastconv.B2S(r), fastconv.B2S(trimExpect))
+			}
 		}
-	}
+	})
+	b.Run("trim str", func(b *testing.B) {
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			r := bytealg.TrimStr(trimOriginS, trimCutS)
+			if r != trimExpectS {
+				b.Errorf(`Trim: mismatch result %s and expectation %s`, r, trimExpectS)
+			}
+		}
+	})
 }
 
-func BenchmarkTrim_Native(b *testing.B) {
-	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
-		r := bytes.Trim(trimOrigin, trimCutStr)
-		if !bytes.Equal(r, trimExpect) {
-			b.Errorf(`Trim: mismatch result %s and expectation %s`, fastconv.B2S(r), fastconv.B2S(trimExpect))
+func BenchmarkNative(b *testing.B) {
+	b.Run("trim", func(b *testing.B) {
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			r := bytes.Trim(trimOrigin, trimCutStr)
+			if !bytes.Equal(r, trimExpect) {
+				b.Errorf(`Trim: mismatch result %s and expectation %s`, fastconv.B2S(r), fastconv.B2S(trimExpect))
+			}
 		}
-	}
+	})
+	b.Run("trim str", func(b *testing.B) {
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			r := strings.Trim(trimOriginS, trimCutS)
+			if r != trimExpectS {
+				b.Errorf(`Trim: mismatch result %s and expectation %s`, r, trimExpectS)
+			}
+		}
+	})
 }
