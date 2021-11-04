9b7635a80833d5d9a50826f6a1e61a373a3e5013
Konstantin Voykov <koykov0@gmail.com>
2021-11-04 17:06:20 +0200

    urlvector versus project: use subtests.

--- urlvector/url_test.go
+++ urlvector/url_test.go
@@ -7,96 +7,95 @@ import (
 	"github.com/koykov/fastconv"
 )
 
-func BenchmarkNetUrl_ParseUrl(b *testing.B) {
-	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
-		u, err := url.Parse(fastconv.B2S(url0))
-		if err != nil {
-			b.Error(err)
-		}
+func BenchmarkNetUrl(b *testing.B) {
+	b.Run("parse url", func(b *testing.B) {
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			u, err := url.Parse(fastconv.B2S(url0))
+			if err != nil {
+				b.Error(err)
+			}
 
-		if u.Scheme != url0scheme {
-			reportErr(b, "scheme", url0scheme, u.Scheme)
+			if u.Scheme != url0scheme {
+				reportErr(b, "scheme", url0scheme, u.Scheme)
+			}
+			if u.User.Username() != url0user {
+				reportErr(b, "username", url0user, u.User.Username())
+			}
+			if p, _ := u.User.Password(); p != url0pass {
+				reportErr(b, "password", url0pass, p)
+			}
+			if u.Host != url0host {
+				reportErr(b, "host", url0host, u.Host)
+			}
+			if u.Path != url0path {
+				reportErr(b, "path", url0path, u.Path)
+			}
+			if u.Fragment != url0fragment {
+				reportErr(b, "hash", url0fragment, u.Fragment)
+			}
 		}
-		if u.User.Username() != url0user {
-			reportErr(b, "username", url0user, u.User.Username())
-		}
-		if p, _ := u.User.Password(); p != url0pass {
-			reportErr(b, "password", url0pass, p)
-		}
-		if u.Host != url0host {
-			reportErr(b, "host", url0host, u.Host)
-		}
-		if u.Path != url0path {
-			reportErr(b, "path", url0path, u.Path)
-		}
-		if u.Fragment != url0fragment {
-			reportErr(b, "hash", url0fragment, u.Fragment)
-		}
-	}
-}
+	})
+	b.Run("parse query", func(b *testing.B) {
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			u, err := url.Parse(fastconv.B2S(url0))
+			if err != nil {
+				b.Error(err)
+			}
 
-func BenchmarkNetUrl_ParseQuery(b *testing.B) {
-	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
-		u, err := url.Parse(fastconv.B2S(url0))
-		if err != nil {
-			b.Error(err)
+			q, err := url.ParseQuery(u.RawQuery)
+			if err != nil {
+				b.Error(err)
+			}
+			if q["page"][0] != url0qPage {
+				b.Error("query[page] mismatch")
+			}
+			if q["ua"][0] != url0qUa {
+				b.Error("query[ua] mismatch")
+			}
+			if q["uid"][0] != url0qUid {
+				b.Error("query[uid] mismatch")
+			}
+			if q["ip"][0] != url0qIp {
+				b.Error("query[ip] mismatch")
+			}
 		}
+	})
+	b.Run("mod host", func(b *testing.B) {
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			u, err := url.Parse(fastconv.B2S(url0))
+			if err != nil {
+				b.Error(err)
+			}
 
-		q, err := url.ParseQuery(u.RawQuery)
-		if err != nil {
-			b.Error(err)
+			u.Host = "x.com:3333"
+			if u.String() != url0modHost {
+				b.Error("mod host mismatch")
+			}
 		}
-		if q["page"][0] != url0qPage {
-			b.Error("query[page] mismatch")
-		}
-		if q["ua"][0] != url0qUa {
-			b.Error("query[ua] mismatch")
-		}
-		if q["uid"][0] != url0qUid {
-			b.Error("query[uid] mismatch")
-		}
-		if q["ip"][0] != url0qIp {
-			b.Error("query[ip] mismatch")
-		}
-	}
-}
+	})
+	b.Run("mod query", func(b *testing.B) {
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			u, err := url.Parse(fastconv.B2S(url0))
+			if err != nil {
+				b.Error(err)
+			}
 
-func BenchmarkNetUrl_ModHost(b *testing.B) {
-	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
-		u, err := url.Parse(fastconv.B2S(url0))
-		if err != nil {
-			b.Error(err)
-		}
+			q, err := url.ParseQuery(u.RawQuery)
+			if err != nil {
+				b.Error(err)
+			}
+			if q["ip"][0] != url0qIp {
+				b.Error("mod query old ip mismatch")
+			}
 
-		u.Host = "x.com:3333"
-		if u.String() != url0modHost {
-			b.Error("mod host mismatch")
+			q, err = url.ParseQuery(url0newQuery)
+			if q["ip"][0] != url0qIp1 {
+				b.Error("mod query new ip mismatch")
+			}
 		}
-	}
-}
-
-func BenchmarkNetUrl_ModQuery(b *testing.B) {
-	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
-		u, err := url.Parse(fastconv.B2S(url0))
-		if err != nil {
-			b.Error(err)
-		}
-
-		q, err := url.ParseQuery(u.RawQuery)
-		if err != nil {
-			b.Error(err)
-		}
-		if q["ip"][0] != url0qIp {
-			b.Error("mod query old ip mismatch")
-		}
-
-		q, err = url.ParseQuery(url0newQuery)
-		if q["ip"][0] != url0qIp1 {
-			b.Error("mod query new ip mismatch")
-		}
-	}
+	})
 }
--- urlvector/urlvector_test.go
+++ urlvector/urlvector_test.go
@@ -6,101 +6,100 @@ import (
 	"github.com/koykov/urlvector"
 )
 
-func BenchmarkUrlvector_ParseUrl(b *testing.B) {
-	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
-		vec := urlvector.Acquire()
-		err := vec.ParseCopy(url0)
-		if err != nil {
-			b.Error(err)
-		}
+func BenchmarkUrlvector(b *testing.B) {
+	b.Run("parse url", func(b *testing.B) {
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			vec := urlvector.Acquire()
+			err := vec.ParseCopy(url0)
+			if err != nil {
+				b.Error(err)
+			}
 
-		if vec.SchemeString() != url0scheme {
-			reportErr(b, "scheme", url0scheme, vec.SchemeString())
-		}
-		if vec.UsernameString() != url0user {
-			reportErr(b, "username", url0user, vec.UsernameString())
-		}
-		if vec.PasswordString() != url0pass {
-			reportErr(b, "password", url0pass, vec.PasswordString())
-		}
-		if vec.HostString() != url0host {
-			reportErr(b, "host", url0host, vec.HostString())
-		}
-		if vec.PathString() != url0path {
-			reportErr(b, "path", url0path, vec.PathString())
-		}
-		if vec.HashString() != url0hash {
-			reportErr(b, "hash", url0hash, vec.HashString())
-		}
+			if vec.SchemeString() != url0scheme {
+				reportErr(b, "scheme", url0scheme, vec.SchemeString())
+			}
+			if vec.UsernameString() != url0user {
+				reportErr(b, "username", url0user, vec.UsernameString())
+			}
+			if vec.PasswordString() != url0pass {
+				reportErr(b, "password", url0pass, vec.PasswordString())
+			}
+			if vec.HostString() != url0host {
+				reportErr(b, "host", url0host, vec.HostString())
+			}
+			if vec.PathString() != url0path {
+				reportErr(b, "path", url0path, vec.PathString())
+			}
+			if vec.HashString() != url0hash {
+				reportErr(b, "hash", url0hash, vec.HashString())
+			}
 
-		urlvector.Release(vec)
-	}
-}
-
-func BenchmarkUrlvector_ParseQuery(b *testing.B) {
-	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
-		vec := urlvector.Acquire()
-		err := vec.ParseCopy(url0)
-		if err != nil {
-			b.Error(err)
+			urlvector.Release(vec)
 		}
+	})
+	b.Run("parse query", func(b *testing.B) {
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			vec := urlvector.Acquire()
+			err := vec.ParseCopy(url0)
+			if err != nil {
+				b.Error(err)
+			}
 
-		query := vec.Query()
-		if query.Get("page").String() != url0qPage {
-			b.Error("query[page] mismatch")
-		}
-		if query.Get("ua").String() != url0qUa {
-			b.Error("query[ua] mismatch")
-		}
-		if query.Get("uid").String() != url0qUid {
-			b.Error("query[uid] mismatch")
-		}
-		if query.Get("ip").String() != url0qIp {
-			b.Error("query[page] mismatch")
-		}
+			query := vec.Query()
+			if query.Get("page").String() != url0qPage {
+				b.Error("query[page] mismatch")
+			}
+			if query.Get("ua").String() != url0qUa {
+				b.Error("query[ua] mismatch")
+			}
+			if query.Get("uid").String() != url0qUid {
+				b.Error("query[uid] mismatch")
+			}
+			if query.Get("ip").String() != url0qIp {
+				b.Error("query[page] mismatch")
+			}
 
-		urlvector.Release(vec)
-	}
-}
-
-func BenchmarkUrlvector_ModHost(b *testing.B) {
-	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
-		vec := urlvector.Acquire()
-		err := vec.ParseCopy(url0)
-		if err != nil {
-			b.Error(err)
+			urlvector.Release(vec)
 		}
+	})
+	b.Run("mod host", func(b *testing.B) {
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			vec := urlvector.Acquire()
+			err := vec.ParseCopy(url0)
+			if err != nil {
+				b.Error(err)
+			}
 
-		vec.SetHostnameString("x.com").SetPort(3333)
-		if vec.String() != url0modHost {
-			b.Error("mod host mismatch")
+			vec.SetHostnameString("x.com").SetPort(3333)
+			if vec.String() != url0modHost {
+				b.Error("mod host mismatch")
+			}
+
+			urlvector.Release(vec)
 		}
+	})
+	b.Run("mod query", func(b *testing.B) {
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			vec := urlvector.Acquire()
+			err := vec.ParseCopy(url0)
+			if err != nil {
+				b.Error(err)
+			}
 
-		urlvector.Release(vec)
-	}
-}
+			if vec.Query().Get("ip").String() != url0qIp {
+				b.Error("mod query old ip mismatch")
+			}
 
-func BenchmarkUrlvector_ModQuery(b *testing.B) {
-	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
-		vec := urlvector.Acquire()
-		err := vec.ParseCopy(url0)
-		if err != nil {
-			b.Error(err)
+			vec.SetQueryString(url0newQuery)
+			if vec.Query().Get("ip").String() != url0qIp1 {
+				b.Error("mod query new ip mismatch")
+			}
+
+			urlvector.Release(vec)
 		}
-
-		if vec.Query().Get("ip").String() != url0qIp {
-			b.Error("mod query old ip mismatch")
-		}
-
-		vec.SetQueryString(url0newQuery)
-		if vec.Query().Get("ip").String() != url0qIp1 {
-			b.Error("mod query new ip mismatch")
-		}
-
-		urlvector.Release(vec)
-	}
+	})
 }
