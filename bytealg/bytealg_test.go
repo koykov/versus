156cf44b8caca90a9e246343c621560d2a9ff05c
Konstantin Voykov <koykov0@gmail.com>
2023-05-01 21:50:13 +0300

    bytealg: upgrade go to 1.18; update deps.

--- bytealg/bytealg_test.go
+++ bytealg/bytealg_test.go
@@ -21,7 +21,7 @@ func BenchmarkBytealg(b *testing.B) {
 	b.Run("trim str", func(b *testing.B) {
 		b.ReportAllocs()
 		for i := 0; i < b.N; i++ {
-			r := bytealg.TrimStr(trimOriginS, trimCutS)
+			r := bytealg.Trim(trimOriginS, trimCutS)
 			if r != trimExpectS {
 				b.Errorf(`Trim: mismatch result %s and expectation %s`, r, trimExpectS)
 			}
