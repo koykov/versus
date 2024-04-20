cd6dafaf4ed035da2cc4b585e5091d424b4f03d0
Konstantin Voykov <koykov0@gmail.com>
2024-04-20 23:43:35 +0300

    inspector2: dynamic reader attempt, v3.1.

--- inspector2/reflect2.go
+++ inspector2/reflect2.go
@@ -18,18 +18,62 @@ func diveR2V2(typ reflect2.Type, path ...string) any {
 	kind := typ.Kind()
 	switch kind {
 	case reflect.Interface:
+		// todo implement me
 	case reflect.Struct:
+		utyp := typ.(*reflect2.UnsafeStructType)
+		for i := 0; i < utyp.NumField(); i++ {
+			field := utyp.Field(i)
+			if field.Name() == path[0] {
+				return diveR2V2(field.Type(), path[1:]...)
+			}
+		}
 	case reflect.Array:
 		// todo implement me
 	case reflect.Slice:
 		// todo implement me
 	case reflect.Map:
 		// todo implement me
-	case reflect.Ptr:
+	case reflect.Pointer:
 		utyp := typ.(*reflect2.UnsafePtrType)
 		elem := utyp.Elem()
 		return diveR2V2(elem, path...)
+	case reflect.Bool:
+	case reflect.Int:
+
+	case reflect.Int8:
+
+	case reflect.Int16:
+
+	case reflect.Int32:
+
+	case reflect.Int64:
+
+	case reflect.Uint:
+
+	case reflect.Uint8:
+
+	case reflect.Uint16:
+
+	case reflect.Uint32:
+
+	case reflect.Uint64:
+
+	case reflect.Uintptr:
+
+	case reflect.Float32:
+
+	case reflect.Float64:
+
+	case reflect.Complex64:
+
+	case reflect.Complex128:
+
+	case reflect.String:
+		var s string
+		a := typ.Indirect(&s)
+		return a
 	default:
+		// reflect.Invalid, reflect.Chan and reflect.UnsafePointer is unsupported.
 		return nil
 	}
 	return nil
