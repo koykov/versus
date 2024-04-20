b3c47393c9aa0c58d9a6087134bab1c4e9e5b8fd
Konstantin Voykov <koykov0@gmail.com>
2024-04-20 22:45:02 +0300

    inspector2: dynamic reader attempt, v3.

--- inspector2/reflect2.go
+++ inspector2/reflect2.go
@@ -1,16 +1,36 @@
 package inspector2
 
-import "github.com/modern-go/reflect2"
+import (
+	"reflect"
+
+	"github.com/modern-go/reflect2"
+)
 
 func diveReflect2(node any, path ...string) any {
 	if len(path) == 0 {
 		return node
 	}
 	typ := reflect2.TypeOf(node)
-	dec := getReaderR2(typ)
-	ptr := reflect2.PtrOf(node)
-	if ptr == nil || dec == nil {
+	return diveR2V2(typ, path...)
+}
+
+func diveR2V2(typ reflect2.Type, path ...string) any {
+	kind := typ.Kind()
+	switch kind {
+	case reflect.Interface:
+	case reflect.Struct:
+	case reflect.Array:
+		// todo implement me
+	case reflect.Slice:
+		// todo implement me
+	case reflect.Map:
+		// todo implement me
+	case reflect.Ptr:
+		utyp := typ.(*reflect2.UnsafePtrType)
+		elem := utyp.Elem()
+		return diveR2V2(elem, path...)
+	default:
 		return nil
 	}
-	return dec.Read(ptr, path...)
+	return nil
 }
