a6f62743ee0d60bf28c2d37a059af6a53e3ce2fb
Konstantin Voykov <koykov0@gmail.com>
2024-04-25 20:35:40 +0300

    inspector2: minor fixes

--- inspector2/inspector_test.go
+++ inspector2/inspector_test.go
@@ -3,12 +3,13 @@ package inspector2
 import (
 	"testing"
 
+	"github.com/koykov/inspector"
 	"github.com/koykov/versus/inspector2/inspector2_ins"
 )
 
 func TestInspector(t *testing.T) {
 	t.Run("obj.L1.L2.L3.S", func(t *testing.T) {
-		var ins types_ins.TInspector
+		ins, _ := inspector.GetInspector("T")
 		x, _ := ins.Get(&obj, "L1", "L2", "L3", "S")
 		t.Log(*x.(*string)) // prints "foobar"
 	})
@@ -17,9 +18,10 @@ func TestInspector(t *testing.T) {
 func BenchmarkInspector(b *testing.B) {
 	b.Run("obj.L1.L2.L3.S", func(b *testing.B) {
 		b.ReportAllocs()
+		var ins types_ins.TInspector
+		var buf any
 		for i := 0; i < b.N; i++ {
-			var ins types_ins.TInspector
-			_, _ = ins.Get(&obj, "L1", "L2", "L3", "S")
+			_ = ins.GetTo(&obj, &buf, "L1", "L2", "L3", "S")
 		}
 	})
 }
